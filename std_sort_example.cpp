1f47384d46e1ddf2400be359402673db0f12c582
L Carver <wifihum@gmail.com>
2023-11-22 17:48:26 -0800

    add __FUNCTION_NAME__ macro, pass it to new display functions

--- std_sort_example.cpp
+++ std_sort_example.cpp
@@ -8,6 +8,15 @@
 #include <iostream>			// std::cout
 #include <vector>			// std::vector
 
+// C++ provides a function name macro, with different names on Windows and *nix
+#ifndef __FUNCTION_NAME__
+	#ifdef _WIN32   //WINDOWS
+		#define __FUNCTION_NAME__   __FUNCTION__  
+	#else          //*NIX
+		#define __FUNCTION_NAME__   __func__ 
+	#endif
+#endif
+
 using namespace std;
 
 //------------------------------------------------------------------------------
@@ -20,20 +29,34 @@ void sort_int_array(int ia[], int num_elements);
 // If the vector is passed by value, changes (like a sort) will not survive
 // the function call.
 
-void sort_char_vector(vector<char>& vc);
+void sort_char_vector(vector<char> vc);
+void display_array_elements(int ia[], int num_elements, const char*, const string&);
+void display_vector_elements(vector<char>& vc, const char*, const string&);
 
 //------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
 int main() {
 
-	int intArray[] = { 35, 10, 93, 85, 62, 77, 345, 43, 2, 10 };
+	// array sort demo
 
+	int intArray[] = { 35, 10, 93, 85, 62, 77, 345, 43, 2, 10 };
+	int num_elements = sizeof(intArray) / sizeof(intArray[0]);
+
+	display_array_elements(intArray, num_elements, __FUNCTION_NAME__, "(): array before sort");
+	
 	// pass starting array address in memory and how many elements
-	sort_int_array(intArray, sizeof(intArray) / sizeof(intArray[0]));
+	sort_int_array(intArray, num_elements);
+
+	display_array_elements(intArray, num_elements, __FUNCTION_NAME__, "(): array after sort");
+
+	// vector sort demo
 
 	vector<char> charVector = { 't', 'o', 'y', ' ', 'b', 'o', 'a', 't' };
+
+	display_vector_elements(charVector, __FUNCTION_NAME__, "(): vector before sort");
 	sort_char_vector(charVector);
+	display_vector_elements(charVector, __FUNCTION_NAME__, "(): vector after sort");
 
 	cout << "Type any key to close this window...";
 	cin.get();
@@ -69,49 +92,59 @@ void sort_int_array(int ia[], int num_elements) {
 	// #TODO sizeof() for demo only
 	int bad_array_size = sizeof(ia) / sizeof(ia[0]);
 
-	cout << "integer array before sorting: ";
-	for (int i = 0; i < num_elements; i++)
-		cout << ia[i] << " ";
-
-	cout << '\n';
-
 	// Here, std::sort() needs to know 2 things:
 	//		1) the starting memory address of the first element 
 	//		2) the ending address of the last element plus one
 
+	display_array_elements(ia, num_elements,
+		__FUNCTION_NAME__, "(): array before sort");
+
 	sort(ia, ia + num_elements);
 
-	cout << "integer array after sorting:  ";
-	for (int i = 0; i < num_elements; i++)
-		cout << ia[i] << " ";
-
-	cout << '\n' << '\n';
+	display_array_elements(ia, num_elements, 
+		__FUNCTION_NAME__, "(): array after sort");
 }
 
 //------------------------------------------------------------------------------
 // sort passed vector of chars
 //------------------------------------------------------------------------------
-void sort_char_vector(vector<char>& vc) {
+void sort_char_vector(vector<char> vc) {
 
 	// We know the passed vector's size inside this function,
 	// and much more, because a vector carries its own information with it.
 
-	cout << "character vector before sorting: ";
+	display_vector_elements(vc, __FUNCTION_NAME__, "(): vector before sort");
 	
-	// C++ knows how many elements in the passed vector, so 
-	// we can use an easy range-based for loop.
-
-	for (char ch : vc)
-		cout << ch;
-
-	cout << '\n';
-
 	// sort() needs to know where the first element and last elements are
 	// in memory. Easy - just use the vector's begin() and end().
 
 	sort(vc.begin(), vc.end());
 
-	cout << "character vector after sorting:  ";
+	display_vector_elements(vc, __FUNCTION_NAME__, "(): vector after sort");
+}
+
+//------------------------------------------------------------------------------
+// display elements of passed array of int
+//------------------------------------------------------------------------------
+void display_array_elements(int ia[], int num_elements, 
+	const char* func_name, const string& debug_msg) {
+
+	cout << func_name << debug_msg << "\n\n";
+
+	for (int i = 0; i < num_elements; i++)
+		cout << ia[i] << " ";
+
+	cout << '\n';
+}
+
+//------------------------------------------------------------------------------
+// display elements of passed vector of chars
+//------------------------------------------------------------------------------
+void display_vector_elements(vector<char>& vc, 
+	const char* func_name, const string& debug_msg) {
+
+	cout << func_name << debug_msg << "\n\n";
+
 	for (char ch : vc)
 		cout << ch;
 
